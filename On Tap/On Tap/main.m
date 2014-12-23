260ae6abc9e163b05f967dc80e5383ac5e9f81a9
jackarendt <jack.arendt93@gmail.com>
2014-12-23 15:14:02 -0600

    Initial Xcode Commit

---
+++ On Tap/On Tap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  On Tap
+//
+//  Created by Jack Arendt on 12/23/14.
+//  Copyright (c) 2014 John Arendt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
